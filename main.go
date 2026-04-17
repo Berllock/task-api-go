764156460a8ffd9d07d6d3d3bcfd85a03c3818b1
Berllock <brenosouzaemp@gmail.com>
2026-04-17 01:01:01 -0300

    start create methods

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"os"
 	"time"
 )
 
@@ -17,6 +19,34 @@ const (
 	StatusInProgress = "in_progress"
 	StatusDone       = "done"
 )
+
+func saveTasks(tasks []Task) error {
+	data, err := json.Marshal(tasks)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile("tasks.json", data, 0644)
+}
+
+//func loadTasks([]Task, error)  {
+//	data, err := os.ReadFile("tasks.json")
+//	if err != nil {
+//		if os.IsNotExist(err) {
+//			return []Task{}, nil
+//		}
+//		return nil, err
+//	}
+//
+//	var tasks []Task
+//
+//	err = json.Unmarshal(data, &tasks)
+//	if err != nil {
+//		return nil, err
+//	}
+//	return tasks, nil
+//
+//}
 
 func main() {
 
