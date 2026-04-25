42dc185ed0fb4e12b8d324956d6b869f792e6780
Breno Souza <breno@itexto.com.br>
2026-04-25 20:41:27 -0300

    Finish project

--- main.go
+++ main.go
@@ -50,6 +50,33 @@ func loadTasks() ([]Task, error) {
 
 }
 
+func changeStatus(args []string, newStatus string) {
+	if len(args) < 3 {
+		fmt.Println("Erro: Forneça o ID da tarefa.")
+		return
+	}
+
+	id, _ := strconv.Atoi(args[2])
+	tasks, _ := loadTasks()
+
+	found := false
+	for i := range tasks {
+		if tasks[i].ID == id {
+			tasks[i].Status = newStatus
+			tasks[i].CreatedAt = time.Now()
+			found = true
+			break
+		}
+	}
+
+	if found {
+		saveTasks(tasks)
+		fmt.Printf("Tarefa %d marcada como %s!\n", id, newStatus)
+	} else {
+		fmt.Printf("Tarefa não encontrada")
+	}
+}
+
 func main() {
 	args := os.Args
 
@@ -91,7 +118,6 @@ func main() {
 		fmt.Printf("Tarefa adicionada com sucesso! (ID: %d)\n", novaTask.ID)
 
 	case "list":
-
 		tasks, err := loadTasks()
 
 		if err != nil {
@@ -99,8 +125,21 @@ func main() {
 			return
 		}
 
+		filter := ""
+		if len(args) > 2 {
+			filter = args[2]
+		}
+
+		found := false
 		for _, task := range tasks {
-			fmt.Println(task)
+			if filter == "" || task.Status == filter {
+				fmt.Printf("[%d] %s = Status %s (Criada em: %v)\n",
+					task.ID, task.Description, task.Status, task.CreatedAt.Format("02/01 15:04"))
+				found = true
+			}
+		}
+		if !found && filter != "" {
+			fmt.Println("Nenhuma tarefa encontrada com o status: %s\n", filter)
 		}
 
 	case "update":
@@ -145,6 +184,50 @@ func main() {
 		}
 
 		fmt.Printf("Tarefa atualizada com sucesso! (ID: %d)\n", id)
+
+	case "mark-in-progress":
+		changeStatus(args, StatusInProgress)
+
+	case "mark-done":
+		changeStatus(args, StatusDone)
+
+	case "delete":
+		if len(args) < 3 {
+			print("Erro: Forneça o ID para deletar a tarefa")
+			return
+		}
+
+		id, err := strconv.Atoi(args[2])
+		if err != nil {
+			print("Erro: ID inválido. Use um numero")
+		}
+
+		tasks, err := loadTasks()
+		if err != nil {
+			fmt.Println("Erro ao carregar tarefas:", err)
+		}
+
+		findTask := false
+		for i := range tasks {
+			if tasks[i].ID == id {
+				tasks = append(tasks[:i], tasks[i+1:]...)
+
+				findTask = true
+				break
+			}
+		}
+
+		if !findTask {
+			fmt.Println("Erro: Tarefa com ID %d não encontrada. \n", id)
+			return
+		}
+
+		err = saveTasks(tasks)
+		if err != nil {
+			fmt.Println("Erro ao salvar tarefa:", err)
+		}
+
+		fmt.Println("Tarefa deletada com sucesso!")
 
 	}
 
